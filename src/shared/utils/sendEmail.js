8d207b4a520958eafcb9ac3d4fbed7d90ff94388
Muhammad-Zainn <zainjamil84@gmail.com>
2026-09-15 16:34:47 +0500

    change the sender email to techryxai.tech

--- src/shared/utils/sendEmail.js
+++ src/shared/utils/sendEmail.js
@@ -18,7 +18,7 @@ const sendWelcomeEmail = async (
 
   try {
     const { data, error } = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>", // Do not change this on the free tier
+      from: "ClientSync <onboarding@techryxai.tech>",
       to: [email],
       subject: `Your account for ${agencyName} is ready`,
       html: `
